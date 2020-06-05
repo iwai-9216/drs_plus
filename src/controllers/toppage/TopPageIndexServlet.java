f25b452b0ddb0dfe3d437043399b96a51ab54d9d
kunihiro.iwai <iwai9216soft@gmail.com>
2020-06-05 18:24:06 +0900

    Add login / logout link

--- src/controllers/toppage/TopPageIndexServlet.java
+++ src/controllers/toppage/TopPageIndexServlet.java
@@ -8,11 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-/*
-import models.Employee;
-import models.Report;
-import utils.DBUtil;
-*/
+
 @WebServlet("/index.html")
 public class TopPageIndexServlet extends HttpServlet {
         private static final long serialVersionUID = 1L;
@@ -47,12 +43,12 @@ public class TopPageIndexServlet extends HttpServlet {
         request.setAttribute("reports", reports);
         request.setAttribute("reports_count", reports_count);
         request.setAttribute("page", page);
-
+*/
         if(request.getSession().getAttribute("flush") != null) {
             request.setAttribute("flush", request.getSession().getAttribute("flush"));
             request.getSession().removeAttribute("flush");
         }
-*/
+
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/topPage/index.jsp");
         rd.forward(request, response);
     }
